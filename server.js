3bc9ccb341893c84859e64e7dd0fc6eaebdf92ec
ptitdam2001 <ptitdam2001@gmail.com>
2015-03-07 10:15:13 +0100

    init node server with express

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require('method-override');
+var jsonApi = require("./json-rest/api.js");
+var config = require('config');
+
+app = express();
+//app.use(bodyParser.urlencoded({ extended: false }); 
+//app.use(bodyParser.json());
+app.use(methodOverride());      // simulate DELETE and PUT
+
+console.log('Server init OK');
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+
+//init routes
+var routes = config.get('routes');
+
+console.log(routes);
+
+app.get(routes[0].url, jsonApi.findAll);
+app.get(routes[1].url, jsonApi.findById)
+
+app.set('port', config.get("port"));
+
+
+server = require("http").createServer(app);
+server.listen(config.get("port"));
